7af9227c99d93343c3f1e1d5cd5d2205001537d2
marcl <marc.c.lang@gmail.com>
2023-02-06 19:17:29 +0000

    adding clone

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ echo **** Installing Dependencies ****
 #sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd lib32z1 libgdiplus certbot python3-certbot-apache
 echo **** Installing Ansible ****
 #sudo apt install -y ansible
-mkdir installer
-git clone -depth 1 https://github.com/xibalbus/lgsm-installer.git installer
+mkdir -p installer
+git clone --depth 1 https://github.com/xibalbus/lgsm-installer.git installer
 echo Running ansible playbook
 cd installer
 ansible-playbook install-rustserver.yaml -i inventory/hosts -vv
