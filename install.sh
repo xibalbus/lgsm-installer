d8600cb6cd23e66a937cc3715939c8a1d1cb8334
marc <marc.c.lang@gmail.com>
2023-02-13 21:14:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 #echo **** Updating package list ****
 #sudo apt update
 echo **** Installing Dependencies ****
-#sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd lib32z1 libgdiplus certbot python3-certbot-apache acl
+sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd lib32z1 libgdiplus certbot python3-certbot-apache acl
 echo **** Installing Ansible ****
-#sudo apt install -y ansible
+sudo apt install -y ansible
 mkdir -p installer
 git clone --depth 1 https://github.com/xibalbus/lgsm-installer.git installer
 echo Running ansible playbook
